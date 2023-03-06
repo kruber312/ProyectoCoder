1665040f49fbabf16adf6857898a2b0ec0225a1f
Hernan Garcia <hernage@gmail.com>
2023-03-06 19:19:46 -0300

    Haciendo Vistas

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -7,3 +7,18 @@ def guardar_curso(request, camada):
     curso = Curso(nombre="HTML",camada=camada,apellido="Coderhouse")
     curso.save()
     return HttpResponse("Usuario creado exitosamente")
+
+def inicio(request):
+    return HttpResponse("Vista Inicio")
+
+def cursos(request):
+    return HttpResponse("Vista Cursos")
+
+def profesores(request):
+    return HttpResponse("Vista Profesores")
+
+def estudiantes(request):
+    return HttpResponse("Vista Estudiantes")
+
+def entregables(request)
+    return HttpResponse("Vista Entregables")
