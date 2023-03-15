e5a3ee2864ec08d6a0e809bade698c47a7d1d773
Hernan Garcia <hernage@gmail.com>
2023-03-14 21:30:33 -0300

    Cambie las carpetas igual que la clase y redireccione todo en index.html

--- ProyectoCoder/settings.py
+++ ProyectoCoder/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProyectoCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "AppCoder/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
