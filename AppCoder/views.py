3ab0c953f7ef26e249991e75bbec5af592d073eb
Hernan Garcia <hernage@gmail.com>
2023-03-15 17:02:23 -0300

    Half Clase 21.
    Formulario para ingresar Cursos completo

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
 from AppCoder.models import Curso
+from AppCoder.forms import CursoForm
 
 # Create your views here.
 def guardar_curso(request, nombre, camada):
@@ -14,9 +15,20 @@ def inicio(request):
     return render(request, "base.html")
 
 def cursos(request):
+    if request.method == "POST":
+        mi_formulario = CursoForm(request.POST)
+
+        if mi_formulario.is_valid():
+            informacion = mi_formulario.cleaned_data
+            curso_save = Curso(
+                nombre=informacion["nombre"],
+                camada=informacion["camada"]
+            )
+            curso_save.save()
     all_cursos = Curso.objects.all()
     context = {
-        "cursos": all_cursos
+        "cursos": all_cursos,
+        "form": CursoForm()
     }
     return render(request, "AppCoder/cursos.html", context=context)
 
