f459e0f7c194905f1573a7e436f6eda1ef6bb73d
Hernan Garcia <hernage@gmail.com>
2023-03-16 20:13:28 -0300

    Bsuqeuda de formularios

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.inicio), #Esta es nuestra primera view
     path('guardarcursos/<nombre>/<camada>', views.guardar_curso),
     path('cursos', views.cursos, name="AppCoderCursos"),
+    path('cursos/eliminar/<camada>', views.eliminar_curso, name="AppCoderEliminar"),
+    path('cursos/editar/<camada>', views.editar_curso, name="AppCoderEditar"),
+    path('cursos/crear', views.crear_curso, name="AppCoderCrearCursos"),
     path('profesores', views.profesores, name="AppCoderProfesores"),
     path('estudiantes', views.estudiantes, name="AppCoderEstudiantes"),
     path('entregables', views.entregables, name="AppCoderEntregables"),
